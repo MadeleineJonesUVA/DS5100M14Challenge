1317ffae45eee322725a5305bdff15222b0a7482
juliadeaver <julia_deaver@icloud.com>
2022-07-11 11:01:05 -0400

    M

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -1,8 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='fortune',
      version = '0.1',
      description = "A great Fortune Teller",
      author = 'Julia, Madeleine, Leah',
      license = 'MIT',
-     packages = ['fortune'])
+     packages = find_packages())
